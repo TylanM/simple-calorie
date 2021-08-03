40e07ebf701065bd44b5733c9930a9da618d13ec
tylanm <tylanmcpherson@gmail.com>
2021-08-03 13:11:39 -0700

    "Second commit"

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Home from './components/home'
+
+
+const App = () => {
+    return (
+        <View style={styles.container}>
+            <Home />
+        </View>
+    )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+})
+
